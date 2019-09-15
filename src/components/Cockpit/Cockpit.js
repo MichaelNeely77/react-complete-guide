30a88d54d9410f85ff251911eb77784480d9daf0
Michael Neely <michael@michaelneely.net>
2019-09-15 00:07:35 -0400

    More on useEffect and cleaning up

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,11 +2,27 @@ import React, { useEffect } from 'react';
 
 import styles from './Cockpit.module.css';
 
-const cockpit = (props) => {
+const cockpit = props => {
 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
+        // Http request...
+        setTimeout(() => {
+            alert('Saved data to the cloud!');
+        }, 1000);
+        return () => {
+
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+        };
     });
+
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
