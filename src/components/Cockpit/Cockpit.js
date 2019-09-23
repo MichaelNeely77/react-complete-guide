5cd0ef643f2745ddba6e2d7b68da241338b4c91f
Michael Neely <michael@michaelneely.net>
2019-09-22 23:00:27 -0400

    Ends React exercises

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import styles from './Cockpit.module.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = props => {
     const toggleBtnRef = useRef(null);
+    const authContext = useContext(AuthContext);
+
+    console.log(authContext.authenticated);
 
 
     useEffect(() => {
@@ -43,9 +46,8 @@ const cockpit = props => {
             <button ref={toggleBtnRef}
             className={btnClass}
             onClick={props.clicked}>Toggle Persons</button>
-            <AuthContext.Consumer>
-                {(context) => <button onClick={context.login}>Log in</button>}
-            </AuthContext.Consumer>
+            <button onClick={authContext.login}>Log in</button>}
+
         </div>
         
     );
